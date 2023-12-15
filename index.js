e35feb306d7502eeda7ef161679af5ac53d5467a
hkaur02 <hkaur@udel.edu>
2023-12-14 23:40:37 -0500

    firebase attempt

--- index.js
+++ index.js
@@ -1,3 +1,29 @@
+var admin = require("firebase-admin");
+
+var serviceAccount = require("path/to/serviceAccountKey.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://exercise-5-40ed8-default-rtdb.firebaseio.com"
+});
+
+// Get a database reference to our blog
+const { getDatabase } = require('firebase-admin/database');
+const db = getDatabase();
+const ref = db.ref('server/saving-data/');
+const usersRef = ref.child('users');
+usersRef.set({
+alanisawesome: {
+date_of_birth: 'June 23, 1912',
+full_name: 'Alan Turing'
+},
+gracehop: {
+date_of_birth: 'December 9, 1906',
+full_name: 'Grace Hopper'
+}
+});
+
+
 const http = require('http')
 const port = 8080
 // Create a server object:
